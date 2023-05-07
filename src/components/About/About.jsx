1128ebd0ea6781dacff9d5a25046c41c7da745b0
Simon Tessard <simon.tessard@gmail.com>
2023-05-07 20:39:19 +0200

    Removed profile picture (replaced with black square)

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,5 @@
 import './about.css'
 
-import profile from '../../assets/images/about-picture.png'
 import ContactButton from '../ContactButton/ContactButton'
 
 import Heading from '../Heading/Heading'
@@ -9,9 +8,10 @@ function About() {
   return (
     <div className="about">
       <Heading beforeText="À" italicText="PRO" afterText="POS" margin={true} />
-      <div className="about-text">
-        <img src={profile} alt="Portrait" />
-        <p>
+      <div className="about-text-container">
+        <div id="whitebg"></div>
+        <div id="blackbg"></div>
+        <div className="aboutText">
           Développeur junior front-end. J'ai obtenu un BTS SIO Option : Développeur. Je recherche
           une alternance. in at rutrum orci. Turpis ullamcorper convallis gravida penatibus gravida
           vitae. Elit morbi in volutpat pulvinar dui venenatis cursus. Eget felis consequat donec
@@ -19,10 +19,10 @@ function About() {
           ipsum nec hendrerit nisl amet eu. Ante placerat etiam neque sed sit. Ultrices vel amet
           vitae dictum semper. Tempor nunc aliquet penatibus in non purus facilisis. In eget elit
           eget nisi quisque diam mattis.
-        </p>
-        <div className="contact-button-1">
-          <ContactButton text="↗ CONTACTEZ-MOI" />
         </div>
+      </div>
+      <div className="contact-button-1">
+        <ContactButton text="↗ CONTACTEZ-MOI" />
       </div>
     </div>
   )
