1dc1ab50ea3ced84d055318ffa5ffd3640a8c6ab
Simon Tessard <simon.tessard@gmail.com>
2023-05-14 19:21:28 +0200

    Improved project card

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,13 +1,16 @@
 import './projectcard.css'
 
 import LinkButton from '../LinkButton/LinkButton'
+import { NavLink } from 'react-router-dom'
 
 function ProjectCard({ title, date, image, url }) {
   return (
     <div className="project">
-      <div className="project-illustration">
-        <img className="project-image" src={image} alt="" />
-      </div>
+      <NavLink to={`/portfolio-2023/project/${url}`}>
+        <div className="project-illustration">
+          <img className="project-image" src={image} alt="" />
+        </div>
+      </NavLink>
       <div className="project-heading">
         <h4>{title}</h4>
         <LinkButton text="↗ VOIR LE PROJET" link={`/portfolio-2023/project/${url}`} />
