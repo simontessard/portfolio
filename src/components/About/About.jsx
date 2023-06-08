8e376575a3bcde3605e8bf2eb7797874f241adb7
Simon Tessard <simon.tessard@gmail.com>
2023-06-08 18:16:19 +0200

    Updated about text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,13 +14,14 @@ function About() {
         <div id="blackbg"></div>
         <p className="aboutText">
           <Reveal>
-            Développeur junior front-end, je suis titulaire d'un BTS SIO (Option Développeur) et
-            d'un Bachelor OpenClassrooms "Développeur d'application - JavaScript React", je me
-            prépare actuellement à poursuivre mes études en Master en alternance. J'ai acquis une
-            expérience en développement web par mon travail sur différents projets. Constamment à la
-            recherche de nouveaux défis pour améliorer mes compétences, j'aspire à participer à des
-            projets dans un milieu professionnel. Si vous êtes à la recherche d'un développeur pour
-            votre entreprise ou votre projet, n'hésitez pas à me contacter.
+            Développeur junior, je suis titulaire d'un BTS SIO (Option Développeur) et d'une
+            formation Bac+3 "Développeur d'application - JavaScript React" chez OpenClassrooms, je
+            me prépare actuellement à poursuivre mes études en réalisant un Bac+5 en alternance
+            toujours chez OpenClassrooms. J'ai acquis une expérience en développement web par mon
+            travail sur différents projets. Constamment à la recherche de nouveaux défis pour
+            améliorer mes compétences, j'aspire à participer à des projets dans un milieu
+            professionnel. Si vous êtes à la recherche d'un développeur pour votre entreprise ou
+            votre projet, n'hésitez pas à me contacter.
           </Reveal>
         </p>
       </div>
